7aae6656a6d25a91831e65cd2ebc223cbd5e4753
Ignacio Colautti <nachitox2000@hotmail.com>
2019-01-05 11:36:47 -0300

    JWT auth added

--- app/User.php
+++ app/User.php
@@ -6,7 +6,9 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -35,5 +37,16 @@ class User extends Authenticatable
 	public function messages()
 	{
 		return $this->hasMany(Message::class);
+	}
+	
+	// JWT
+	public function getJWTIdentifier()
+	{
+		return $this->getKey();
+	}
+	
+	public function getJWTCustomClaims()
+	{
+		return [];
 	}
 }
